1aceb42ab2f000e2a4ad3d53e1e6576216de54e6
Vika <viktoria.likhotkina@gmail.com>
2017-12-21 22:20:19 +0200

    remove libft

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlikhotk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/08 13:06:44 by vlikhotk          #+#    #+#             */
+/*   Updated: 2017/11/08 13:06:47 by vlikhotk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*dest;
+	unsigned int	i;
+
+	i = 0;
+	dest = NULL;
+	if (s && f)
+	{
+		s = (char *)s;
+		while (s[i] != 0)
+			i++;
+		if (!(dest = malloc(sizeof(char) * (i + 1))))
+			return (NULL);
+		i = 0;
+		while (s[i] != 0)
+		{
+			dest[i] = f(i, (char)s[i]);
+			i++;
+		}
+		dest[i] = 0;
+		return (dest);
+	}
+	return (NULL);
+}
---
+++ libft/ft_strnew.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlikhotk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/08 13:05:53 by vlikhotk          #+#    #+#             */
+/*   Updated: 2017/11/08 13:05:57 by vlikhotk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char	*dest;
+	size_t	i;
+
+	i = 0;
+	dest = NULL;
+	if (!(dest = malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	while (i < size)
+	{
+		dest[i] = 0;
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
