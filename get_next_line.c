7a2b49129e27affff452051712ee1a3740ca095e
Viktoriia LIKHOTKINA <vlikhotk@e1r3p13.unit.ua>
2018-01-10 14:20:30 +0200

    All tests passed exclude multi fd

--- get_next_line.c
+++ get_next_line.c
@@ -13,6 +13,44 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
+char *if_next(char **line, char **tmp, char **next_str)
+{
+	int i;
+	//printf("NEXT1 %s\n", *next_str);
+	i = 0;
+	if (*line)
+	{
+		free(*line);
+		*line = NULL;
+	}
+	while ((*next_str)[i] != '\n' && (*next_str)[i] != '\0')
+		i++;
+	//printf("1EXAMPLE %s\n", *line);
+	if ((*next_str)[i] == '\n')
+	{
+		//printf("2EXAMPLE %s\n", *line);
+		*line = ft_strsub((*next_str),  0, i);
+		//printf("2EXAMPLE %s\n", *line);
+		if (i != (int)ft_strlen((*next_str)))
+		{
+			(*next_str) = ft_strsub((*next_str), i + 1, ft_strlen((*next_str)) - i);
+		}
+		else
+			if ((*next_str))
+			{
+				free((*next_str));
+				(*next_str) = NULL;
+			}
+	}
+	else
+	{
+		(*tmp) = ft_strjoin((*tmp), (*next_str));
+		free((*next_str));
+		(*next_str) = NULL;
+	}
+	return (*line);
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	int		i;
@@ -25,7 +63,6 @@ int		get_next_line(const int fd, char **line)
 	i = 0;
 	j = 0;
 	tmp = "";
-	buf[BUFF_SIZE] = 0;
 	if (fd < 0 || !line || read(fd, buf, 0) < 0 || BUFF_SIZE < 1)
 	{
 		//printf("RES RET%d\n", -1);
@@ -33,60 +70,31 @@ int		get_next_line(const int fd, char **line)
 	}
 	*line = NULL;
 	ret = 1;
+	if (next_str)
+	{
+		//printf("NEXT1 %s\n", next_str);
+		if (if_next(line, &tmp, &next_str))
+			return (1);
+	}
 	while (ret > 0)
 	{
-		if (next_str)
+		i = 0;
+		ft_strclr(buf);
+		ret = read(fd, &buf, BUFF_SIZE);
+		buf[ret] = 0;
+		if (ret == 0)
 		{
-			//printf("NEXT1 %s\n", next_str);
-			i = 0;
-			while (next_str[i] != '\n' && next_str[i] != '\0')
-				i++;
-			//printf("1EXAMPLE %s\n", *line);
-			if (next_str[i] == '\n')
+			if (tmp[0] != 0)
 			{
-				//printf("2EXAMPLE %s\n", *line);
-				*line = ft_strsub(next_str,  0, i);
-				//printf("2EXAMPLE %s\n", *line);
-				if (i != (int)ft_strlen(next_str))
-				{
-					next_str = ft_strsub(next_str, i + 1, ft_strlen(next_str) - i);
-				}
-				else
-					if (next_str)
-					{
-						free(next_str);
-						next_str = NULL;
-					}
-
+				*line = ft_strsub(tmp, 0, ft_strlen(tmp));
 				ret = 1;
 				break;
 			}
 			else
-			{
-				tmp = ft_strjoin(tmp, next_str);
-				free(next_str);
-				next_str = NULL;
-			}
+				return (ret);
 		}
-		i = 0;
-		ft_strclr(buf);
-		ret = read(fd, &buf, BUFF_SIZE);
-		if (ret == 0)
-			return (ret);
-		/*else
-		{
-			ret = 1;
-			continue;
-		}*/
 		//printf("1FIRST BUF %s\n", buf);
 		//printf("RET%d\n", ret);
-		/*if (ret == 0)
-			return (ret);*/
-		/*else if (ret == 0)
-		{
-			ret = 1;
-			continue;
-		}*/
 		if (ret > 0)
 		{
 			while (i < ret && buf[i] != '\n')
@@ -119,6 +127,11 @@ int		get_next_line(const int fd, char **line)
 			}
 			else
 			{
+				if (*line)
+				{
+					free(*line);
+					*line = NULL;
+				}
 				tmp = ft_strjoin(tmp, buf);
 				//printf("ELSE TMP %s\n", tmp);
 			}
